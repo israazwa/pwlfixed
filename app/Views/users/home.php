b4fba3b4bd8fe83e20e0c6de78a6b802790511f0
israsaqoba <111202315287@mhs.dinus.ac.id>
2025-04-15 15:40:45 +0700

    check

--- app/Views/users/home.php
+++ app/Views/users/home.php
@@ -158,9 +158,4 @@
     }, 7000); // Setiap 7 detik
 </script>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-    crossorigin="anonymous"></script>
-
-
 </html>
